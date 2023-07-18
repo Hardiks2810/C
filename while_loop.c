a2eb48a4e84449c2371e06019c24736b1dd709b3
Hardiks2810 <139541979+Hardiks2810@users.noreply.github.com>
2023-07-18 11:21:07 +0530

    Add files via upload

---
+++ while_loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+// While loop
+// While loop is an entry controlled loop
+int main()
+{
+	int i=1;
+	
+	while(i<=10)
+	{
+		printf("While loop %d\n",i);
+		i++;
+	}
+	return 0;	
+}
